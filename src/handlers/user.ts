829ffa063ee1b521e4b219b256247f35c904a546
CSM-intaniger <tanakornp@cleversecorp.com>
2023-11-01 17:26:42 +0700

    fix(user): :safety_vest: request validation and appropiate error handling

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,3 +1,4 @@
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { RequestHandler } from "express";
 import { IUserHandler } from ".";
 import { IErrorDto } from "../dto/error";
@@ -19,25 +20,46 @@ export default class UserHandler implements IUserHandler {
   > = async (req, res) => {
     const { name, username, password: plainPassword } = req.body;
 
-    const {
-      id: registeredId,
-      name: registeredName,
-      registeredAt,
-      username: registeredUsername,
-    } = await this.repo.create({
-      name,
-      username,
-      password: hashPassword(plainPassword),
-    });
+    if (typeof name !== "string" || name.length === 0)
+      return res.status(400).json({ message: "name is invalid" });
+    if (typeof username !== "string" || username.length === 0)
+      return res.status(400).json({ message: "username is invalid" });
+    if (typeof plainPassword !== "string" || plainPassword.length < 5)
+      return res.status(400).json({ message: "password is invalid" });
 
-    return res
-      .status(201)
-      .json({
+    try {
+      const {
         id: registeredId,
         name: registeredName,
-        registeredAt: `${registeredAt}`,
+        registeredAt,
         username: registeredUsername,
-      })
-      .end();
+      } = await this.repo.create({
+        name,
+        username,
+        password: hashPassword(plainPassword),
+      });
+
+      return res
+        .status(201)
+        .json({
+          id: registeredId,
+          name: registeredName,
+          registeredAt: `${registeredAt}`,
+          username: registeredUsername,
+        })
+        .end();
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === "P2002"
+      ) {
+        return res.status(500).json({
+          message: `name is invalid`,
+        });
+      }
+      return res.status(500).json({
+        message: `Internal Server Error`,
+      });
+    }
   };
 }
