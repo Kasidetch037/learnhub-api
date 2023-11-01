6943c48acf4665a1d30a0843fae74e21629699c8
CSM-intaniger <tanakornp@cleversecorp.com>
2023-11-01 16:45:27 +0700

    feat(user): :sparkles: user registration endpoint

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,3 +1,43 @@
+import { RequestHandler } from "express";
 import { IUserHandler } from ".";
+import { IErrorDto } from "../dto/error";
+import { ICreateUserDto, IUserDto } from "../dto/user";
+import { IUserRepository } from "../repositories";
+import { hashPassword } from "../utils/bcrypt";
 
-export default class UserHandler implements IUserHandler {}
+export default class UserHandler implements IUserHandler {
+  private repo: IUserRepository;
+
+  constructor(repo: IUserRepository) {
+    this.repo = repo;
+  }
+
+  public registration: RequestHandler<
+    {},
+    IUserDto | IErrorDto,
+    ICreateUserDto
+  > = async (req, res) => {
+    const { name, username, password: plainPassword } = req.body;
+
+    const {
+      id: registeredId,
+      name: registeredName,
+      registeredAt,
+      username: registeredUsername,
+    } = await this.repo.create({
+      name,
+      username,
+      password: hashPassword(plainPassword),
+    });
+
+    return res
+      .status(201)
+      .json({
+        id: registeredId,
+        name: registeredName,
+        registeredAt: `${registeredAt}`,
+        username: registeredUsername,
+      })
+      .end();
+  };
+}
